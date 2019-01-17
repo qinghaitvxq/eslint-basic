0d344ef296d0cdf98894f52aef485394dd61f2c4
yunxi <598616202@qq.com>
2019-01-17 23:01:45 +0800

    t

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const bar = 10;
 const foo = bar + 2;
 
 const test = {
-  name: 'test',
-  number: 12
+  name: "test",
+  number: 12,
+  people: {
+    name: "penny",
+    address: "china"
+  }
 };
+console.log("test");
+const t2 = "heelo";
 
 document.write(foo);
 document.write(test);
